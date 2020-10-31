a743aa318825a1d6f027da748f3ebfabd6ef6cc6
Natesh Narain <nnaraindev@gmail.com>
2020-10-31 12:44:52 -0400

    Configure baud

--- include/libfirmware/platform/atmega2560.hpp
+++ include/libfirmware/platform/atmega2560.hpp
@@ -81,30 +81,41 @@ struct HardwareUart
 
     HardwareUart(Baud baud)
     {
-        const auto uart_config = libfirmware::platform::avr::configUart9600();
+        libfirmware::platform::avr::BaudConfig uart_config;
 
-        _SFR_IO16(Regs::UBRR) = uart_config.baud_value;
+        switch (baud)
+        {
+        case Baud::Rate_9600:
+            uart_config = libfirmware::platform::avr::configUart9600();
+            break;
+        case Baud::Rate_115200:
+            uart_config = libfirmware::platform::avr::configUart115200();
+        default:
+            break;
+        }
+
+        _SFR_MEM16(Regs::UBRR) = uart_config.baud_value;
 
         if (uart_config.use_2x)
         {
-            SET_BIT(_SFR_IO8(Regs::UCSRA), U2X0);
+            SET_BIT(_SFR_MEM8(Regs::UCSRA), U2X0);
         }
         else
         {
-            CLR_BIT(_SFR_IO8(Regs::UCSRA), U2X0);
+            CLR_BIT(_SFR_MEM8(Regs::UCSRA), U2X0);
         }
 
-        // Enable RX and TX
-        _SFR_IO8(Regs::UCSRB) = _BV(RXEN0) | _BV(TXEN0);
+        // // Enable RX and TX
+        _SFR_MEM8(Regs::UCSRB) = _BV(RXEN0) | _BV(TXEN0);
 
-        // frame format
-        _SFR_IO8(Regs::UCSRC) = _BV(UCSZ01) | _BV(UCSZ00);
+        // // frame format
+        _SFR_MEM8(Regs::UCSRC) = _BV(UCSZ01) | _BV(UCSZ00);
     }
 
     void write(uint8_t data)
     {
-        loop_until_bit_is_set(_SFR_IO8(Regs::UCSRA), UDRE0);
-        _SFR_IO8(Regs::UDR) = data;
+        loop_until_bit_is_set(_SFR_MEM8(Regs::UCSRA), UDRE0);
+        _SFR_MEM8(Regs::UDR) = data;
     }
 
     void write(const uint8_t* data, unsigned int size)
--- include/libfirmware/platform/avr/setbaud.hpp
+++ include/libfirmware/platform/avr/setbaud.hpp
@@ -18,6 +18,7 @@ namespace avr
    struct BaudConfig
    {
       BaudConfig(uint16_t b, bool u) : baud_value{b}, use_2x{u} {}
+      BaudConfig() : BaudConfig(0, false) {}
       uint16_t baud_value;
       bool use_2x;
    };
@@ -37,20 +38,20 @@ namespace avr
 #undef BAUD
    }
 
-//    static BaudConfig configUart115200()
-//    {
-// #define BAUD 115200
-// #include <util/setbaud.h>
-//          constexpr auto b = UBRR_VALUE;
-//          constexpr auto u = 
-// #if USE_2X
-//         true;
-// #else
-//         false;
-// #endif
-//         return BaudConfig{b, u};
-// #undef BAUD
-//    }
+   static BaudConfig configUart115200()
+   {
+#define BAUD 115200
+#include <util/setbaud.h>
+         constexpr auto b = UBRR_VALUE;
+         constexpr auto u = 
+#if USE_2X
+        true;
+#else
+        false;
+#endif
+        return BaudConfig{b, u};
+#undef BAUD
+   }
 }
 }
 }
