3e08dc78baab38772531962a9347469e01ba1db0
Natesh Narain <nnaraindev@gmail.com>
2020-11-29 17:41:31 -0500

    Draw XBitmap file

--- include/ftl/gfx/display.hpp
+++ include/ftl/gfx/display.hpp
@@ -28,7 +28,7 @@ namespace gfx
         /**
          * Set a pixel in the display
         */
-        virtual void drawPixel(unsigned int col, unsigned int row, const Color& pixel) = 0;
+        virtual void drawPixel(unsigned int col, unsigned int row, const Color& c) = 0;
 
         /**
          * Update the display
@@ -72,18 +72,103 @@ namespace gfx
         }
 
         /**
+         * Draw a vertical line
+        */
+        virtual void drawVLine(int x0, int y0, int h, const Color& c)
+        {
+            drawLine(x0, y0, x0, y0 + h, c);
+        }
+
+        /**
+         * Draw a horizontal line
+        */
+        virtual void drawHLine(int x0, int y0, int w, const Color& c)
+        {
+            drawLine(x0, y0, x0 + w, y0, c);
+        }
+
+        /**
          * Draw a rectangle
         */
         virtual void drawRect(int x0, int y0, int w, int h, const Color& c)
         {
             // Draw top
-            drawLine(x0, y0, x0 + w, y0, c);
+            drawHLine(x0, y0, w, c);
             // Draw bottom
-            drawLine(x0, y0 + h, x0 + w, y0 + h, c);
+            drawHLine(x0, y0 + h, w, c);
             // Draw left
-            drawLine(x0, y0, x0, y0 + h, c);
+            drawVLine(x0, y0, h, c);
             // Draw right
-            drawLine(x0 + w, y0, x0 + w, y0 + h, c);
+            drawVLine(x0 + w, y0, h, c);
+        }
+
+        /**
+         * Draw fill rectangle
+        */
+        virtual void drawFillRect(int x0, int y0, int w, int h, const Color& c)
+        {
+            const auto x1 = x0 + w;
+            const auto y1 = y0 + h;
+            for (auto x = x0; x <= x1; ++x)
+            {
+                for (auto y = y0; y <= y1; ++y)
+                {
+                    drawPixel(x, y, c);
+                }
+            }
+        }
+
+        // /**
+        //  * Draw circle
+        // */
+        // virtual void drawCircle(int xc, int yc, int r, const Color& c)
+        // {
+        //     const auto x = 0;
+        //     const auto y = r;
+        // }
+
+        /**
+         * Draw a bitmap in XBITMAP format
+        */
+        virtual void drawXBitmap(const uint8_t* const bitmap, int x, int y, int w, int h, const Color& c)
+        {
+            unsigned int idx = 0;
+            uint8_t byte = bitmap[idx++];
+
+            const auto y1 = y + h;
+            const auto x1 = x + w;
+
+            // Iterate over rows
+            for (auto row = y; row < y1; ++row)
+            {
+                auto scan = 0;
+                // Iterate over columns
+                for (auto col = x; col < x1; ++col)
+                {
+                    if (byte & 0x01)
+                    {
+                        drawPixel(col, row, c);
+                    }
+
+                    scan++;
+
+                    if ((col + 1) & 0x07)
+                    {
+                        byte >>= 1;
+                    }
+                    else
+                    {
+                        byte = bitmap[idx++];
+                    }
+                }
+
+                // Extra bit not used in the byte are ignored
+                if (w % 8 != 0)
+                {
+                    byte = bitmap[idx++];
+                }
+            }
+
         }
 
     private:
---
+++ include/ftl/math/vector.hpp
@@ -0,0 +1,25 @@
+//
+// vector.hpp
+//
+// @author Natesh Narain <nnaraindev@gmail.com>
+// @date Nov 26 2020
+//
+#ifndef FTL_MATH_VECTOR_HPP
+#define FTL_MATH_VECTOR_HPP
+
+namespace ftl
+{
+namespace math
+{
+
+template<typename T>
+struct Vector2
+{
+    T x;
+    T y;
+};
+
+}
+}
+
+#endif
