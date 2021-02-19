4068b8de195d4e9a3945281d7da664796d2551a3
Natesh Narain <nnaraindev@gmail.com>
2021-02-18 21:57:58 -0500

    flash script

---
+++ examples/avr/atmega32u4_bringup/main.cpp
@@ -0,0 +1,29 @@
+//
+// SSD1306 driver for ATmega2560
+//
+// @author Natesh Narain <nnaraindev@gmail.com>
+// @date Nov 21 2020
+//
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include <ftl/logging/logger.hpp>
+#include <ftl/platform/avr/atmega32u4/hardware.hpp>
+
+using namespace ftl::logging;
+using namespace ftl::platform::avr::atmega32u4;
+
+int main()
+{
+
+
+    for(;;)
+    {
+    }
+
+    return 0;
+}
---
+++ include/ftl/platform/avr/atmega32u4/hardware.hpp
@@ -0,0 +1,31 @@
+//
+// platform/avr/atmega32u4/hardware.hpp
+//
+// @author Natesh Narain <nnaraindev@gmail.com>
+// @date Feb 18 2021
+//
+
+#ifndef FTL_PLATFORM_AVR_ATMEGA32U4_HPP
+#define FTL_PLATFORM_AVR_ATMEGA32U4_HPP
+
+namespace ftl
+{
+namespace platform
+{
+namespace avr
+{
+namespace atmega32u4
+{
+    /**
+     * ATmega32u4 Hardware Definition
+    */
+    struct Hardware
+    {
+
+    };
+}
+}
+}
+}
+
+#endif // FTL_PLATFORM_AVR_ATMEGA32U4_HPP
